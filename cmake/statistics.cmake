e7274e764a80d1815cbca3e4344d776fefbb7069
Stefan Hamminga <s@stefanhamminga.com>
2018-09-22 16:45:48 +0300

    Add statistics

---
+++ cmake/statistics.cmake
@@ -0,0 +1,23 @@
+# Cloc based source stats
+find_program(cloc_EXECUTABLE cloc DOC "cloc counts lines of code")
+
+# Cloc directories/files
+set(__CLOC__LOCATIONS
+  ${CMAKE_CURRENT_SOURCE_DIR}/cmake
+  ${CMAKE_CURRENT_SOURCE_DIR}/src/CGUL
+  ${CMAKE_CURRENT_SOURCE_DIR}/src/CGUL.h
+)
+
+# Add custom targets
+add_custom_target("cloc" COMMAND
+  "${cloc_EXECUTABLE}"
+  ${__CLOC__LOCATIONS}
+)
+add_custom_target("clocfiles" COMMAND
+  "${cloc_EXECUTABLE}"
+  ${__CLOC__LOCATIONS}
+  --by-file
+)
+
+
+# Stack depth analysis (port from Teensy cmake kit)
